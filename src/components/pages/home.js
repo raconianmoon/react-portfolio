6c2f5e38a787e9142d0255a310dbda5e8a663bf9
William Ormond <ormonwil000@alpine.local>
2019-07-01 10:44:04 -0600

    Commit

---
+++ src/components/pages/home.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+import PortfolioContainer from "../portfolio/portfolio-container";
+
+export default function() {
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <PortfolioContainer />
+    </div>
+  );
+}
---
+++ src/components/portfolio/portfolio-item.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function(props) {
+  // Data that we'll need:
+  // - background image: thumb_image_url
+  // - logo
+  // - description: description
+  // - id: id
+
+  const { id, description, thumb_image_url, logo } = props.item;
+  return (
+    <div>
+      <img src={thumb_image_url}/>
+      <div>{description}</div>
+      <Link to={`/portfolio/${id}`}>Link</Link>
+    </div>
+  );
+}
